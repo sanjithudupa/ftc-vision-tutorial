2054c87f4fa03970d79dd8623405b3d6461b02a2
sanjithudupa <31494156+sanjithudupa@users.noreply.github.com>
2020-06-19 17:16:02 -0400

    added files

---
+++ SkyStone-master-vision/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuforiaTest.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+@TeleOp(name="Vuforia Test")
+public class VuforiaTest extends LinearOpMode {
+
+    Camera camera = new Camera(hardwareMap);
+
+    RobotPosition robotPosition;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        camera.initialize();
+        camera.initTrackables();
+
+        waitForStart();
+
+        sleep(2500);
+
+        telemetry.addLine("starting scanning now");
+
+        telemetry.update();
+
+        robotPosition = camera.getCurrentPosition(isStopRequested());
+
+        telemetry.addData("X", robotPosition.x);
+        telemetry.addData("Y", robotPosition.y);
+        telemetry.addData("Rot", robotPosition.rot);
+
+
+    }
+}
